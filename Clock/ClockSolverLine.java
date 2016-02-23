6925e2045dbc5fe5a8b0f0bfdcd03ebe0f9e39d5
bdavs3 <bdavis42@lion.lmu.edu>
2016-02-22 17:19:14 -0800

    Revamped whole class
    Originally, I had been calculating the angle in this class, which was very wrong.  I made sure to do that in the clock class.  Thus, I was able to delete a bunch of state variables and methods from this class.  Also, my main method was way too cluttered, so I did all the "solving" in the solver constructor, which is much more sensible.

--- Clock/ClockSolverLine.java
+++ Clock/ClockSolverLine.java
@@ -1,39 +1,24 @@
 public class ClockSolverLine {
     private static double DEFAULT_TIME_SLICE = 60.0;
-    private static double DEGREES_IN_AN_HOUR = 30;
-    private static double DEGREES_IN_A_MIN = 6;
-    private static double MIN_HAND_DEGREES_PER_SEC = 0.1;
-    private static double HOUR_HAND_DEGREES_PER_MIN = 0.5;
-    private static double HOUR_HAND_DEGREES_PER_SEC = 0.5 / 60;
     private static double STRAIGHT_ANGLE = 180.0;
     private static long numberOfStraightAngles = 0;
     private static double timeSlice;
 
     public ClockSolverLine(Clock clock, double timeSlice) {
-        clock.tick(timeSlice);
-    }
+        while(!clock.getCompleteRotation()) {
+            clock.tick(timeSlice);
 
-    public static double getAngle(Clock clock) {
-        double hourAngle = 0;
-        double minuteAngle = 0;
+            String angle = Double.toString(Math.round(clock.getAngle() * 10.0) / 10.0);
+            long hours = clock.getHours() == 0 ? 12 : clock.getHours();
+            String minutes = clock.getMinutes() < 10 ? "0" + Long.toString(clock.getMinutes()) : Long.toString(clock.getMinutes());
+            String seconds = Double.toString(Math.round(clock.getSeconds() * 10.0) / 10.0);
 
-        double hours = clock.getHours();
-        double min = clock.getMinutes();
-        double sec = clock.getSeconds();
-
-        hourAngle += hours * DEGREES_IN_AN_HOUR;
-        hourAngle += min * HOUR_HAND_DEGREES_PER_MIN;
-        hourAngle += sec * HOUR_HAND_DEGREES_PER_SEC;
-        minuteAngle += min * DEGREES_IN_A_MIN;
-        minuteAngle += sec * MIN_HAND_DEGREES_PER_SEC;
-
-        double hourToMinAngle = Math.abs(hourAngle - minuteAngle);
-        double minToHourAngle = Math.abs(minuteAngle - hourAngle);
-        return hourToMinAngle < minToHourAngle ? hourToMinAngle : minToHourAngle;
-    }
-
-    public static double changePerTick(double timeSlice) {
-        return Math.abs(HOUR_HAND_DEGREES_PER_SEC * timeSlice - MIN_HAND_DEGREES_PER_SEC * timeSlice);
+            if (clock.formsDesiredAngle(STRAIGHT_ANGLE)) {
+                numberOfStraightAngles++;
+                System.out.println("Angle of " + angle + " occurs at " + clock.toString()); 
+            }
+        }
+        System.out.println("There are " + numberOfStraightAngles + " straight angles.");
     }
 
     public static void main(String[] args) {
@@ -51,21 +36,8 @@ public class ClockSolverLine {
                 throw new IllegalArgumentException();
             }
         }
-        timeSlice  = args.length == 0 ? DEFAULT_TIME_SLICE : Double.parseDouble(args[0]);
-
-        while(!clock.getCompleteRotation()) {
-            new ClockSolverLine(clock, timeSlice);
-            String angle = Double.toString(Math.round(getAngle(clock) * 10.0) / 10.0);
-            long hours = clock.getHours() == 0 ? 12 : clock.getHours();
-            String minutes = clock.getMinutes() < 10 ? "0" + Long.toString(clock.getMinutes()) : Long.toString(clock.getMinutes());
-            String seconds = Double.toString(Math.round(clock.getSeconds() * 10.0) / 10.0);
-            
-            if (Math.abs(STRAIGHT_ANGLE - getAngle(clock)) < changePerTick(timeSlice) && clock.getHours() < 12) {
-                numberOfStraightAngles++;
-                new ClockSolverLine(clock, timeSlice);
-                System.out.println("Angle of " + angle + " occurs at " + Long.toString(hours) + ":" + minutes + ":" + seconds + "."); 
-            }
-        }
-        System.out.println("There are " + numberOfStraightAngles + " straight angles.");
+        timeSlice = args.length == 0 ? DEFAULT_TIME_SLICE : Double.parseDouble(args[0]);
+        clock.degreesPerTick(timeSlice);
+        new ClockSolverLine(clock, timeSlice);
     }
 }
