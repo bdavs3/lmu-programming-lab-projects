987c6a40f9832e81da57dc3d0cab1d669ce4d3bf
bdavs3 <bdavis42@lion.lmu.edu>
2016-02-22 17:14:42 -0800

    Added methods
    I realized it is much easier have the angle between the hands as a state variable in the Clock class (rather than in the ClockSolverLine and ClockSolverAngle classes).  Thus, I calculated the change in angle per tick in this class.
    
    I also added a few helpful methods in this class to adjust for this change.
    
    Additionally, I edited the toString() method to be more clear.

--- Clock/Clock.java
+++ Clock/Clock.java
@@ -3,8 +3,10 @@ public class Clock {
     private static long SEC_IN_A_MIN = 60;
     private static long MIN_IN_AN_HOUR = 60;
     private static long HOURS_ON_A_CLOCK = 12;
+    private static double MIN_HAND_DEGREES_PER_SEC = 0.1;
+    private static double HOUR_HAND_DEGREES_PER_MIN = 0.5 / 60;
     private long minutes, hours;
-    private double seconds;
+    private double seconds, angle, degreesEachTick;
     private boolean completeRotation = false;
 
     public Clock() {
@@ -15,33 +17,53 @@ public class Clock {
 
     public void tick(double slice) {
         this.seconds += slice;
+        angle += this.degreesEachTick;
+        if (angle >= 360) {
+            angle -= 360;
+        }
+
         if (this.seconds >= SEC_IN_A_MIN) {
             setMinutes((long) (this.minutes + Math.floor(this.seconds / SEC_IN_A_MIN)));
             setSeconds(this.seconds % SEC_IN_A_MIN);
         }
-
         if (this.minutes >= MIN_IN_AN_HOUR) {
             setHours(hours + 1);
             setMinutes(this.minutes % MIN_IN_AN_HOUR);
         }
-
         if (this.hours >= HOURS_ON_A_CLOCK) {
             completeRotation = true;
         }
     }
 
-    public String toString() {
-        return ("Minutes: " + this.minutes + "\n" +
-                "Hours: " + this.hours + "\n" +
-                "Seconds: " + this.seconds + "\n");
+    public void degreesPerTick(double slice) {
+        this.degreesEachTick = Math.abs(HOUR_HAND_DEGREES_PER_MIN * slice - MIN_HAND_DEGREES_PER_SEC * slice);
     }
 
-    public long getMinutes() {
-        return this.minutes;
+    public double getDegreesEachTick() {
+        return this.degreesEachTick;
+    }
+
+    public double getAngle() {
+        return this.angle;
+    }
+
+    public boolean formsDesiredAngle(double angle) {
+        return (Math.abs(angle - this.getAngle()) < this.degreesEachTick / 2 && this.hours < 12);
+    }
+
+    public String toString() {
+        long hours = this.hours == 0 ? 12 : this.hours;
+        String minutes = this.minutes < 10 ? "0" + Long.toString(this.minutes) : Long.toString(this.minutes);
+        String seconds = Double.toString(Math.round(this.seconds * 10.0) / 10.0);
+        return (Long.toString(hours) + ":" + minutes + ":" + seconds + ".");
     }
 
     public long getHours() {
         return this.hours;
+    }
+
+    public long getMinutes() {
+        return this.minutes;
     }
 
     public double getSeconds() {
