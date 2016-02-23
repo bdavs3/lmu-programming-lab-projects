77233e0049fd7d3e9b6e7e51af44c9ef223b5ec7
bdavs3 <bdavis42@lion.lmu.edu>
2016-02-22 21:55:26 -0800

    *read additional comments*
    -Removed two parameters from the constructor (state variables can be used instead)
    -Fixed a number formatting issue with the angle (it was printing out a lot of decimal places sometimes)
    -Added comments
    -Replaced e.g. timeSlice with this.timeSlice in certain places

--- Clock/ClockSolverAngle.java
+++ Clock/ClockSolverAngle.java
@@ -1,26 +1,36 @@
+//PLEASE NOTE
+//There is an inherant issue with input involving this class,
+//specifically instances where the user enters a rather small
+//angle using a rather large timeslice.  In these cases,
+//certain instances of that angle are skipped over.
+
 public class ClockSolverAngle {
     private static double DEFAULT_TIME_SLICE = 60.0;
     private static long numberOfDesiredAngles;
     private static double desiredAngle;
     private static double timeSlice;
 
-    public ClockSolverAngle(Clock clock, double desiredAngle, double timeSlice) {
+    //"do while" is used to ensure that 12:00 is checked before the clock starts ticking.
+    //this constructor is similar to ClockSolverLine, except it also accounts for inverse angles,
+    //which are not relevant for the 180 degree angles found in the ClockSolverLine class
+    public ClockSolverAngle(Clock clock) {
         do {
             String angle = Double.toString(Math.round(clock.getAngle() * 10.0) / 10.0);
-            String inverseAngle = Double.toString(360 - Math.round(clock.getAngle() * 10.0) / 10.0);
-            if (clock.formsDesiredAngle(desiredAngle)) {
-                numberOfDesiredAngles++;
+            String inverseAngle = Double.toString(Math.round((360.0 - clock.getAngle()) * 10.0) / 10.0);
+            if (clock.formsDesiredAngle(this.desiredAngle)) {
+                this.numberOfDesiredAngles++;
                 System.out.println("Angle of " + angle + " occurs at " + clock.toString()); 
-            } else if (clock.formsDesiredAngle(360 - desiredAngle)) {
-                numberOfDesiredAngles++;
+            } else if (clock.formsDesiredAngle(360 - this.desiredAngle)) {
+                this.numberOfDesiredAngles++;
                 System.out.println("Angle of " + inverseAngle + " occurs at " + clock.toString());
             }
 
-            clock.tick(timeSlice);
+            clock.tick(this.timeSlice);
         } while(!clock.getCompleteRotation());
-        System.out.println("There are " + numberOfDesiredAngles + " " + desiredAngle + " degree angles.");
+        System.out.println("There are " + this.numberOfDesiredAngles + " " + this.desiredAngle + " degree angles.");
     }
 
+    //catches and throws exceptions for all kinds of invalid input
     public static void main(String[] args) {
         Clock clock = new Clock();
         try {
@@ -47,7 +57,7 @@ public class ClockSolverAngle {
         }
         desiredAngle = Double.parseDouble(args[0]);
         timeSlice  = args.length == 1 ? DEFAULT_TIME_SLICE : Double.parseDouble(args[1]);
-        clock.degreesPerTick(timeSlice);
-        new ClockSolverAngle(clock, desiredAngle, timeSlice);
+        clock.setDegreesPerTick(timeSlice);
+        new ClockSolverAngle(clock);
     }
 }
