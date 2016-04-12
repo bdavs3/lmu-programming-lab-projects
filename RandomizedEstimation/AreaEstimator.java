478653f9012a6453700eb0dd95decf84ead47bf6
bdavs3 <bdavis42@lion.lmu.edu>
2016-04-12 13:43:17 -0700

    Completely revamped the class
    *Added an additional constructor which serves as the array-organizer
    *Added comments
    *Added some final variables for cleanliness
    *Changed a few void methods to return something (e.g. this.determineLowerBound() )
    *Changed some variables so that they are not instance variables; rather, the only appear within the method that needs them
    *Because "length" (the needed length for the shape array) was an instance variable, it had to be changed to a parameter for one of the constructors, with the new format.  Now, the checkInput() method always returns that length.

--- RandomizedEstimation/AreaEstimator.java
+++ RandomizedEstimation/AreaEstimator.java
@@ -1,36 +1,20 @@
 public class AreaEstimator {
-    private String errorMessage = "For a circle, enter 'circle (centerX) (centerY) (radius)' and for a triangle, enter 'triangle (p1x) (p1y) (p2x) (p2y) (p3x) (p3y)'";
+    private String errorMessage = "For a circle, enter 'circle (centerX) (centerY) (radius)', for a triangle, enter 'triangle (p1x) (p1y) (p2x) (p2y) (p3x) (p3y)', and for a convex polygon, enter 'polygon (number_of_sides) (p1x) (p1y) (p2x) (p2y)...' in clockwise order";
     private Shape[] shapes;
-    private double[] shapeAreas;
     private int dartsInIntersection = 0, dartsInUnion = 0, dartsInNonIntersection = 0;
-    private final int ONE_MILLION = 1000000;
-    private int length = 0, placeInArray = 0, interval;
-    private Point lowerBound, upperBound;
+    private final int ONE_MILLION = 1000000, PARAMETERS_FOR_CIRCLE = 4, PARAMETERS_FOR_TRIANGLE = 7;
 
+    //Necessary for the purposes of invoking a method which tests input and determines how long the shapes array will be
     public AreaEstimator() {
 
     }
 
-    public void checkInput(String[] a) {
-        if (a.length < 4) {
-            throw new IllegalArgumentException(this.errorMessage);
-        }
-
-        for (int i = 0; i < a.length; i += interval) {
-            if (a[i].equals("circle")) {
-                this.length++;
-                this.interval = 4;
-            } else if (a[i].equals("triangle")) {
-                this.length++;
-                this.interval = 7;
-            } else {
-                throw new IllegalArgumentException(this.errorMessage);
-            }
-        }
-    }
-
-    public void createArray(String[] a) {
-        this.shapes = new Shape[this.length];
+    //Sorts the shapes neatly into an array with a length that has been determined by checkInputAndLength()
+    //...Also checks for illegal arguments involving the parameter / characteristics of the different shapes
+    public AreaEstimator(String[] a, int l) {
+        this.shapes = new Shape[l];
+        int interval;
+        int placeInArray = 0;
 
         for (int i = 0; i < a.length; i += interval) {
             if (a[i].equals("circle")) {
@@ -38,19 +22,32 @@ public class AreaEstimator {
                     Point center = new Point(Double.parseDouble(a[i + 1]), Double.parseDouble(a[i + 2]));
                     double radius = Double.parseDouble(a[i + 3]);
                     this.shapes[placeInArray] = new Circle(center, radius);
-                    this.placeInArray++;
-                    this.interval = 4;
+                    placeInArray++;
+                    interval = PARAMETERS_FOR_CIRCLE;
                 } catch (Exception e) {
                     throw new IllegalArgumentException(this.errorMessage);
                 }
-            } else {
+            } else if (a[i].equals("triangle")) {
                 try {
                     Point p1 = new Point(Double.parseDouble(a[i + 1]), Double.parseDouble(a[i + 2]));
                     Point p2 = new Point(Double.parseDouble(a[i + 3]), Double.parseDouble(a[i + 4]));
                     Point p3 = new Point(Double.parseDouble(a[i + 5]), Double.parseDouble(a[i + 6]));
                     this.shapes[placeInArray] = new Triangle(p1, p2, p3);
-                    this.placeInArray++;
-                    this.interval = 7;
+                    placeInArray++;
+                    interval = PARAMETERS_FOR_TRIANGLE;
+                } catch (Exception e) {
+                    throw new IllegalArgumentException(this.errorMessage);
+                }
+            } else {
+                try {
+                    int numberOfSides = Integer.parseInt(a[i + 1]);
+                    Point[] pArr = new Point[numberOfSides];
+                    for (int j = 0; j < numberOfSides; j++) {
+                        pArr[j] = new Point(Double.parseDouble(a[i + (j * 2) + 2]), Double.parseDouble(a[i + (j * 2) + 3]));
+                    }
+                    this.shapes[placeInArray] = new Polygon(pArr);
+                    placeInArray++;
+                    interval = (numberOfSides * 2) + 2;
                 } catch (Exception e) {
                     throw new IllegalArgumentException(this.errorMessage);
                 }
@@ -59,7 +56,40 @@ public class AreaEstimator {
 
     }
 
-    public void determineLowerBound() {
+    //Simply returns what the length of the shape array should be... This is difficult because each shape
+    //requires a different number of parameters.  As the length it calculated, the program checks for
+    //illegal arguments
+    public int checkInputAndLength(String[] a) {
+        int interval;
+        int length = 0;
+
+        if (a.length < PARAMETERS_FOR_CIRCLE) {                         //least amount of parameters possible with this program
+            throw new IllegalArgumentException(this.errorMessage);
+        }
+
+        for (int i = 0; i < a.length; i += interval) {
+            if (a[i].equals("circle")) {
+                length++;
+                interval = PARAMETERS_FOR_CIRCLE;
+            } else if (a[i].equals("triangle")) {
+                length++;
+                interval = PARAMETERS_FOR_TRIANGLE;
+            } else if (a[i].equals("polygon")) {
+                length++;
+                try {
+                    interval = (2 * Integer.parseInt(a[i + 1])) + 2;
+                } catch (Exception e) {
+                    throw new IllegalArgumentException(this.errorMessage);
+                }
+            } else {
+                throw new IllegalArgumentException(this.errorMessage);
+            }
+        }
+
+        return length;
+    }
+
+    public Point determineLowerBound() {
         double minX = this.shapes[0].minBound().getXCoord();
         double minY = this.shapes[0].minBound().getYCoord();
         for (Shape s : this.shapes) {
@@ -73,10 +103,10 @@ public class AreaEstimator {
             }
         }
 
-        this.lowerBound = new Point(minX, minY);
+        return new Point(minX, minY);
     }
 
-    public void determineUpperBound() {
+    public Point determineUpperBound() {
         double maxX = this.shapes[0].maxBound().getXCoord();
         double maxY = this.shapes[0].maxBound().getYCoord();
         for (Shape s : this.shapes) {
@@ -90,21 +120,21 @@ public class AreaEstimator {
             }
         }
 
-        this.upperBound = new Point(maxX, maxY);
+        return new Point(maxX, maxY);
     }
 
     public void displayBoundingBox() {
         System.out.println("Bounding box:");
-        System.out.println("- Upper-right corner: (" + upperBound.getXCoord() + ", " + upperBound.getYCoord() + ")");
-        System.out.println("- Lower-left corner: (" + lowerBound.getXCoord() + ", " + lowerBound.getYCoord() + ")");
+        System.out.println("- Upper-right corner: (" + this.determineUpperBound().getXCoord() + ", " + determineUpperBound().getYCoord() + ")");
+        System.out.println("- Lower-left corner: (" + this.determineLowerBound().getXCoord() + ", " + determineLowerBound().getYCoord() + ")");
     }
 
     public void throwDarts() {
         for (int i = 0; i < ONE_MILLION; i++) {
-            Point dartThrow = Point.random(this.lowerBound, this.upperBound)
-;            boolean addToIntersection = true;
-            boolean addToUnion = false;
-            boolean addToNonIntersection = false;
+            Point dartThrow = Point.random(this.determineLowerBound(), this.determineUpperBound())
+;           boolean addToIntersection = true;     //initialized to true and changed to false if any given dart does not exist in all of the shapes
+            boolean addToUnion = false;           //initialized to false and changed to true once a dart has been found to exist in any single shape
+            boolean addToNonIntersection = false; //initialized to false; changed to true when a dart exists in one single shape, and changed back to false if the dart exists in any additional shapes
             int count = 0;
             for (Shape s : this.shapes) {
                 if (s.insideShape(dartThrow)) {
@@ -126,8 +156,8 @@ public class AreaEstimator {
         }
     }
 
-    public void displayShapeAreas() {
-        shapeAreas = new double[this.length];
+    public void displayShapeAreas(int l) {
+        double [] shapeAreas = new double[l];
         int i = 0;
         for (Shape s : this.shapes) {
             shapeAreas[i] = this.areaWithDarts(s.getNumberOfDarts());
@@ -145,24 +175,29 @@ public class AreaEstimator {
         System.out.println("Area of non-intersection: " + areaOfNonIntersection);
     }
 
+    //This output stream is intended to be piped into Dartboard.java... Yes, this means that this output steam
+    //involves different darts than the ones that were used to calculate area.  This is because the output stream
+    //takes quite a bit of time to be sent to the console.  This way, area can be calculated instantaneously,
+    //and then the slow-natured output is saved for last.
     public void output() {
         System.out.println("start");
         for (int i = 0; i < ONE_MILLION; i++) {
-            Point dartThrow = Point.random(this.lowerBound, this.upperBound);
+            Point dartThrow = Point.random(this.determineLowerBound(), this.determineUpperBound());
             boolean inShape = false;
             for (Shape s : this.shapes) {
                 if (s.insideShape(dartThrow)) {
                     inShape = true;
                 }
             }
-            System.out.println(dartThrow.getXCoord() + " " + dartThrow.getYCoord() + (inShape ? " in" : " out"));
+            System.out.println(dartThrow.toString() + (inShape ? " in" : " out"));
         }
         System.out.println("end");
     }
 
+    //Necessary for calculating the area of a shape with a given number of darts in it
     public double areaOfBound() {
-        double base = this.upperBound.getXCoord() - this.lowerBound.getXCoord();
-        double height = this.upperBound.getYCoord() - this.lowerBound.getYCoord();
+        double base = this.determineUpperBound().getXCoord() - this.determineLowerBound().getXCoord();
+        double height = this.determineUpperBound().getYCoord() - this.determineLowerBound().getYCoord();
 
         return base * height;
     }
@@ -172,14 +207,12 @@ public class AreaEstimator {
     }
 
     public static void main(String[] args) {
-        AreaEstimator ae = new AreaEstimator();
-        ae.checkInput(args);
-        ae.createArray(args);
-        ae.determineLowerBound();
-        ae.determineUpperBound();
+        AreaEstimator input = new AreaEstimator();
+        int length = input.checkInputAndLength(args);
+        AreaEstimator ae = new AreaEstimator(args, length);
         ae.displayBoundingBox();
         ae.throwDarts();
-        ae.displayShapeAreas();
+        ae.displayShapeAreas(length);
         ae.output();
     }
 }
