6aa943b0801442a619b98a79e840eb6a7c1649cf
bdavs3 <bdavis42@lion.lmu.edu>
2016-04-12 01:57:23 -0700

    Fully functional.
    Everything works as intended, but I still need to clean up code.  Also, I'm probably gonna add polygon support.

--- RandomizedEstimation/AreaEstimator.java
+++ RandomizedEstimation/AreaEstimator.java
@@ -2,6 +2,7 @@ public class AreaEstimator {
     private String errorMessage = "For a circle, enter 'circle (centerX) (centerY) (radius)' and for a triangle, enter 'triangle (p1x) (p1y) (p2x) (p2y) (p3x) (p3y)'";
     private Shape[] shapes;
     private double[] shapeAreas;
+    private int dartsInIntersection = 0, dartsInUnion = 0, dartsInNonIntersection = 0;
     private final int ONE_MILLION = 1000000;
     private int length = 0, placeInArray = 0, interval;
     private Point lowerBound, upperBound;
@@ -92,6 +93,73 @@ public class AreaEstimator {
         this.upperBound = new Point(maxX, maxY);
     }
 
+    public void displayBoundingBox() {
+        System.out.println("Bounding box:");
+        System.out.println("- Upper-right corner: (" + upperBound.getXCoord() + ", " + upperBound.getYCoord() + ")");
+        System.out.println("- Lower-left corner: (" + lowerBound.getXCoord() + ", " + lowerBound.getYCoord() + ")");
+    }
+
+    public void throwDarts() {
+        for (int i = 0; i < ONE_MILLION; i++) {
+            Point dartThrow = Point.random(this.lowerBound, this.upperBound)
+;            boolean addToIntersection = true;
+            boolean addToUnion = false;
+            boolean addToNonIntersection = false;
+            int count = 0;
+            for (Shape s : this.shapes) {
+                if (s.insideShape(dartThrow)) {
+                    s.incrementNumberOfDarts();
+                    addToUnion = true;
+                    if (count == 0) {
+                        addToNonIntersection = true;
+                        count++;
+                    } else {
+                        addToNonIntersection = false;
+                    }
+                } else {
+                    addToIntersection = false;
+                }
+            }
+            this.dartsInIntersection += addToIntersection ? 1 : 0;
+            this.dartsInUnion += addToUnion ? 1 : 0;
+            this.dartsInNonIntersection += addToNonIntersection ? 1 : 0;
+        }
+    }
+
+    public void displayShapeAreas() {
+        shapeAreas = new double[this.length];
+        int i = 0;
+        for (Shape s : this.shapes) {
+            shapeAreas[i] = this.areaWithDarts(s.getNumberOfDarts());
+            System.out.println("Area of shape " + (i + 1) + ": " + shapeAreas[i]);
+            i++;
+        }
+
+        double areaOfIntersection = this.areaWithDarts(this.dartsInIntersection);
+        System.out.println("Area of intersection: " + areaOfIntersection);
+
+        double areaOfUnion = this.areaWithDarts(this.dartsInUnion);
+        System.out.println("Area of union: " + areaOfUnion);
+
+        double areaOfNonIntersection = this.areaWithDarts(this.dartsInNonIntersection);
+        System.out.println("Area of non-intersection: " + areaOfNonIntersection);
+    }
+
+    public void output() {
+        System.out.println("start");
+        for (int i = 0; i < ONE_MILLION; i++) {
+            Point dartThrow = Point.random(this.lowerBound, this.upperBound);
+            boolean inShape = false;
+            for (Shape s : this.shapes) {
+                if (s.insideShape(dartThrow)) {
+                    inShape = true;
+                }
+            }
+            System.out.println(dartThrow.getXCoord() + " " + dartThrow.getYCoord() + (inShape ? " in" : " out"));
+        }
+        System.out.println("end");
+    }
+
     public double areaOfBound() {
         double base = this.upperBound.getXCoord() - this.lowerBound.getXCoord();
         double height = this.upperBound.getYCoord() - this.lowerBound.getYCoord();
@@ -99,42 +167,8 @@ public class AreaEstimator {
         return base * height;
     }
 
-    public void throwDarts() {
-        for (int i = 0; i < ONE_MILLION; i++) {
-            Point dartThrow = Point.random(this.lowerBound, this.upperBound);
-            for (Shape s : this.shapes) {
-                if (s.insideShape(dartThrow)) {
-                    s.incrementNumberOfDarts();
-                }
-            }
-        }
-    }
-
-    public void calculateShapeAreas() {
-        shapeAreas = new double[this.length];
-        int i = 0;
-        for (Shape s : this.shapes) {
-            shapeAreas[i] = (s.getNumberOfDarts() / (double) ONE_MILLION) * this.areaOfBound();
-            System.out.println(shapeAreas[i]);
-            i++;
-        }
-    }
-
-    public void output() {
-        for (int i = 0; i < ONE_MILLION; i++) {
-            Point dartThrow = Point.random(this.lowerBound, this.upperBound);
-            System.out.println("start");
-            for (Shape s : this.shapes) {
-                inner:
-                if (s.insideShape(dartThrow)) {
-                    System.out.println(dartThrow.getXCoord() + " " + dartThrow.getYCoord() + " in");
-                    break inner;
-                } else {
-                    System.out.println(dartThrow.getXCoord() + " " + dartThrow.getYCoord() + " out");
-                }
-            }
-            System.out.println("end");
-        }
+    public double areaWithDarts(int numOfDarts) {
+        return (numOfDarts / (double) ONE_MILLION) * this.areaOfBound();
     }
 
     public static void main(String[] args) {
@@ -143,8 +177,9 @@ public class AreaEstimator {
         ae.createArray(args);
         ae.determineLowerBound();
         ae.determineUpperBound();
+        ae.displayBoundingBox();
         ae.throwDarts();
-        ae.calculateShapeAreas();
+        ae.displayShapeAreas();
         ae.output();
     }
 }
