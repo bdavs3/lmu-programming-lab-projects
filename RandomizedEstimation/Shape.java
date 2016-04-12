c0d4ca14c2cb9ede8f6787e5e67e0ab318d29225
bdavs3 <bdavis42@lion.lmu.edu>
2016-04-11 23:09:32 -0700

    Added a couple of abstract methods

--- RandomizedEstimation/Shape.java
+++ RandomizedEstimation/Shape.java
@@ -2,4 +2,6 @@ public abstract class Shape {
     abstract Point maxBound();
     abstract Point minBound();
     abstract boolean insideShape(Point p);
+    abstract void incrementNumberOfDarts();
+    abstract int getNumberOfDarts();
 }
