9971c77dd1092a0fa149b8d87e5bf6a39c0a235b
bdavs3 <bdavis42@lion.lmu.edu>
2016-04-20 20:38:47 -0700

    Added tests!

--- GeneralizedChangemaker/GeneralizedChangemakerTestHarness.java
+++ GeneralizedChangemaker/GeneralizedChangemakerTestHarness.java
@@ -8,7 +8,7 @@ public class GeneralizedChangemakerTestHarness {
         successes = 0;
 
         test_USA();
-        // Add more!
+        test_other();
 
         System.out.println(successes + "/" + attempts + " tests passed.");
     }
@@ -25,6 +25,8 @@ public class GeneralizedChangemakerTestHarness {
     }
 
     public static void test_USA() {
+        System.out.println("Testing USA coins...");
+
         int[] usaDenominations = new int[] { 25, 10, 5, 1 };
 
         Tuple result = GeneralizedChangemaker.makeChangeWithDynamicProgramming(usaDenominations, 99);
@@ -51,6 +53,124 @@ public class GeneralizedChangemakerTestHarness {
 
         try {
             displaySuccessIfTrue(4 == result.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+    }
+
+    public static void test_other() {
+        System.out.println("Testing other denominations...");
+
+        int[] denom0 = new int[] { 3, 6, 8, 11 };
+        int[] denom1 = new int[] { 20, 3, 2, 5 };
+        int[] denom2 = new int[] { 6, 2, 4, 3, 17, 7 };
+        int[] denom3 = new int[] { 3, 7 };
+
+        Tuple result0 = GeneralizedChangemaker.makeChangeWithDynamicProgramming(denom0, 87); 
+        Tuple result1 = GeneralizedChangemaker.makeChangeWithDynamicProgramming(denom1, 42);
+        Tuple result2 = GeneralizedChangemaker.makeChangeWithDynamicProgramming(denom2, 65);
+        Tuple result3 = GeneralizedChangemaker.makeChangeWithDynamicProgramming(denom3, 11);
+        try {
+            displaySuccessIfTrue(0 == result0.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result0.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(4 == result0.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(5 == result0.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(2 == result1.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result1.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == result1.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result1.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result2.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result2.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result2.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == result2.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(3 == result2.getElement(4));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(2 == result2.getElement(5));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(result3.isImpossible());
         } catch (Exception e) {
             e.printStackTrace();
             displayFailure();
